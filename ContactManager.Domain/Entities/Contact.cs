47ce6b6dea01c273afca70317095e91204b2e9f5
hrytsykvl <gricikvlad@gmail.com>
2024-09-12 09:53:30 +0300

    Implemented Clean Architecture, added Contact entity

---
+++ ContactManager.Domain/Entities/Contact.cs
@@ -0,0 +1,11 @@
+﻿namespace ContactManager.Domain.Entities
+{
+    public class Contact
+    {
+        public string Name { get; set; }
+        public DateOnly BirthDate { get; set; }
+        public bool Married { get; set; }
+        public string Phone { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
